0ed6a4f15fb3296e5cc9034b27609e9e5857b505
Joel Hassan <joelhassan@Joels-MBP.home>
2019-06-19 21:20:04 +0300

    1.1 done

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,78 @@
+import React from 'react'; // library: component base
+import ReactDOM from 'react-dom'; // glue connectiing React to DOM API
+
+/* React components function like JS functions
+They take in 'props' and return React elements */
+
+// Header component - page title
+
+const Header = props => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+// Content component - course info
+
+const Content = props => {
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
+  );
+};
+
+// Total component - total number of exercises
+
+const Total = props => {
+  return (
+    <div>
+      <p>
+        Number of exercises{' '}
+        {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  );
+};
+
+// root component - top of component tree
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
