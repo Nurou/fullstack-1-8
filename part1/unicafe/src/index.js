58c02aa8cb6b0de780945639910c9a9c6fd8136f
Joel Hassan <joel.nhn@gmail.com>
2019-06-29 23:17:29 +0300

    refactoring unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,47 +2,48 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Header = props => {
-  return <h1>{props.text}</h1>;
+const Header = ({ text }) => {
+  return <h1>{text}</h1>;
 };
 
-const Button = props => {
-  return <button onClick={props.onClick}>{props.text}</button>;
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
 };
 
 // shows a single statistic
-const Statistic = props => {
+const Statistic = ({ text, value }) => {
   return (
     <tr>
-      <td>{props.text}</td>
-      <td>{props.value}</td>
+      <td>{text}</td>
+      <td>{value}</td>
     </tr>
   );
 };
 
 // combines the statistics
-const Statistics = props => {
-  // extracting the feedback object from props
-  let feedback = props.feedback;
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
 
   // has feedback been given?
-  if (!feedback.total) {
+  if (!total) {
     return <p>No feedback given.</p>;
   }
+
+  // percentage of feedback that was positive
+  const positivePercentage = () => Math.round((good / total) * 100) + ' %' || 0;
+  // the average score (good: 1, neutral: 0, bad: -1)
+  const averageFigure = () => (good - (bad * 1) / total).toPrecision(2);
 
   // if yes, display them in tabular form
   return (
     <table className="darkTable">
       <tbody>
-        <Statistic text="Good" value={feedback.good} />
-        <Statistic text="Neutral" value={feedback.neutral} />
-        <Statistic text="Bad" value={feedback.bad} />
-        <Statistic text="All" value={feedback.total} />
-        <Statistic text="Average" value={feedback.average().toPrecision(2)} />
-        <Statistic
-          text="Positive"
-          value={(Math.round(feedback.positive()) || 0) + ' %'}
-        />
+        <Statistic text="Good" value={good} />
+        <Statistic text="Neutral" value={neutral} />
+        <Statistic text="Bad" value={bad} />
+        <Statistic text="All" value={total} />
+        <Statistic text="Average" value={averageFigure()} />
+        <Statistic text="Positive" value={positivePercentage()} />
       </tbody>
     </table>
   );
@@ -55,45 +56,14 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  // functions for each button
-  const incrementGood = value => {
-    setGood(value);
-  };
-
-  const incrementNeutral = value => {
-    setNeutral(value);
-  };
-
-  const incrementBad = value => {
-    setBad(value);
-  };
-
-  // feedback data
-  const feedbackStats = {
-    good: good,
-    neutral: neutral,
-    bad: bad,
-    total: good + neutral + bad,
-    // the average score (good: 1, neutral: 0, bad: -1)
-    average() {
-      let totalPoints = good - bad * 1;
-      let feedbackCount = this.total;
-      return totalPoints / feedbackCount;
-    },
-    // percentage of feedback that was positive
-    positive() {
-      return (good / this.total) * 100;
-    }
-  };
-
   return (
     <div>
       <Header text="Give your feedback!" />
-      <Button onClick={() => incrementGood(good + 1)} text="Good" />
-      <Button onClick={() => incrementNeutral(neutral + 1)} text="Neutral" />
-      <Button onClick={() => incrementBad(bad + 1)} text="Bad" />
+      <Button onClick={() => setGood(good + 1)} text="Good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="Bad" />
       <Header text="Statistics: " />
-      <Statistics feedback={feedbackStats} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
