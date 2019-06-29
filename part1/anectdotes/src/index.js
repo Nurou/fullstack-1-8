ab72d78ba7a5d0a3b58516b7f9eec30586e6c33f
Joel Hassan <joel.nhn@gmail.com>
2019-06-29 20:56:16 +0300

    1.14 refactoring

--- part1/anectdotes/src/index.js
+++ part1/anectdotes/src/index.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-//TODO: add a button that can be clicked to display a random anecdote from the field of software engineering:
-
 const Button = props => {
   return <button onClick={props.onClick}>{props.text}</button>;
 };
@@ -12,46 +10,28 @@ const Header = props => {
 };
 
 const DisplayMostVoted = props => {
-  // extract votes array passed in
+  // extract votes array passed in as props
   const votes = props.votes;
-  // get the index of the array element having the most votes
+  // retrieve index of the array element having the most votes
   const indexOfMostVoted = votes.indexOf(Math.max(...votes));
+  const mostVotedAnecdote = anecdotes[indexOfMostVoted];
+  const voteCount = votes[indexOfMostVoted];
 
   return (
     <div>
-      {anecdotes[indexOfMostVoted]}
+      {mostVotedAnecdote}
       <br />
-      has {votes[indexOfMostVoted]} votes
+      has {voteCount} votes
     </div>
   );
 };
 
-const App = props => {
-  // quote selection is based on selected array index
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
-
-  const selectRandomAnecdote = props => {
-    // random quote from elements 0 - 5
-    setSelected(Math.floor(Math.random() * 5));
-  };
-
-  // forbidden in React to mutate state directly (), hence
-  // why a copy of the array is used
-  const voteForAnecdote = props => {
-    const updatedVotes = [...votes];
-    updatedVotes[props] += 1;
-    setVotes(updatedVotes);
-  };
-
+const DisplayCurrent = props => {
   return (
     <div>
-      <Header text="Anecdote of the day" />
-      {props.anecdotes[selected]}
-      <Button onClick={() => voteForAnecdote(selected)} text="vote" />
-      <Button onClick={() => selectRandomAnecdote()} text="next anecdote" />
-      <Header text="Anecdote with most votes" />
-      <DisplayMostVoted votes={votes} />
+      {props.anecdote}
+      <br />
+      has {props.votes} votes
     </div>
   );
 };
@@ -64,5 +44,41 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ];
+
+const App = props => {
+  const anecdotes = props.anecdotes;
+
+  // quote selection is based on selected array index
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0));
+
+  const selectRandomAnecdote = props => {
+    // random quote from elements 0 - 5
+    setSelected(Math.floor(Math.random() * 5));
+  };
+
+  // forbidden in React to mutate state directly ()
+  // hence the use of a copy
+  const voteForAnecdote = props => {
+    const updatedVotes = [...votes];
+    updatedVotes[props] += 1;
+    setVotes(updatedVotes);
+  };
+
+  // consts to be used for display of anecdote data
+  const anecdoteText = anecdotes[selected];
+  const anecdoteVoteCount = votes[selected];
+
+  return (
+    <div>
+      <Header text="Anecdote of the day" />
+      <DisplayCurrent anecdote={anecdoteText} votes={anecdoteVoteCount} />
+      <Button onClick={() => voteForAnecdote(selected)} text="vote" />
+      <Button onClick={() => selectRandomAnecdote()} text="next anecdote" />
+      <Header text="Anecdote with most votes" />
+      <DisplayMostVoted votes={votes} />
+    </div>
+  );
+};
 
 ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
