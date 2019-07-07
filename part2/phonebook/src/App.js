2ac290f3a2131f0cbc6e1cdba67a4257c60e4c64
Joel Hassan <joel.nhn@gmail.com>
2019-07-07 16:53:43 +0300

    2.11 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,37 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Person from './components/person';
 import Filter from './components/filter';
 import PersonForm from './components/personForm';
 import Persons from './components/persons';
+import axios from 'axios';
 
 const App = () => {
   // application state pieces
-  const [persons, setPersons] = useState([
-    {
-      id: 1,
-      name: 'Arto Hellas',
-      number: '040-123456'
-    },
-    {
-      id: 2,
-      name: 'Ada Lovelace',
-      number: '39-44-5323523'
-    },
-    {
-      id: 3,
-      name: 'Dan Abramov',
-      number: '12-43-234345'
-    },
-    {
-      id: 4,
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122'
-    }
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchInput, setSearchInput] = useState('');
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons') //
+      .then(response => {
+        setPersons(response.data);
+      });
+  };
+
+  useEffect(hook, []);
 
   // filtering persons based on user search input
   const personsToList = persons.filter(person => {
